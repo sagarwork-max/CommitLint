758665ce53b8ab561bb1423701d3132229a92f8c
sagarrathod17 <90125157+sagarrathod17@users.noreply.github.com>
2025-01-22 23:11:48 +0530

    feat: add new API endpoint

---
+++ commitlint.config.js
@@ -0,0 +1,7 @@
+﻿module.exports = {
+    extends: ['@commitlint/config-conventional'],
+    rules: {
+        'type-enum': [2, 'always', ['feat', 'fix', 'docs', 'style', 'refactor', 'perf', 'test']],
+        'header-max-length': [2, 'always', 72]
+    }
+};
