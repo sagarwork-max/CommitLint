dde18a20ff5cfefd6af3a03d36a52559857ff21d
sagarrathod17 <90125157+sagarrathod17@users.noreply.github.com>
2025-01-22 22:44:48 +0530

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CommitLint.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
